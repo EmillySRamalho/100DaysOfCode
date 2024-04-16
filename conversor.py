491a9ed1bd73fbbd36ea1655383fb01644c30467
Emilly Santos Ramalho <83793954+EmillySRamalho@users.noreply.github.com>
2024-04-16 19:30:45 -0300

    Add files via upload

---
+++ conversor.py
@@ -0,0 +1,15 @@
+# Conversos de temperatura
+
+#Entrada de dados 
+
+def celsiusParaFahrenheit(celsius): 
+  return (celsius * 9/5) + 32
+
+def fahreinheitParaCelsius(Fareinheit):
+  return(Fareinheit -32) * 5/9
+
+print(celsiusParaFahrenheit(25))
+print(fahreinheitParaCelsius(77))
+print(celsiusParaFahrenheit(15))
+print(fahreinheitParaCelsius(59))
+
---
+++ conversorTemperatura.py
@@ -0,0 +1,12 @@
+#Conversor de temperatura
+
+celsius = int(input("Digite um valor em Celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+print("A temperatura em Fahrenheit é:", fahrenheit)
+
+fahrenheit = int(input("Digite um valor em Fahrenheit: "))
+celsius = (fahrenheit - 32) * 5/9
+print("A temperatura em Celsius é:", celsius)
+
+
+
