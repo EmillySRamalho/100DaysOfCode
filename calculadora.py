16856d2d358b67ccbcb42c2d31fd8e835ded7e8a
Emilly Santos Ramalho <emilly_bsfpb@hotmail.com>
2024-04-15 21:38:08 -0300

    codigo

---
+++ calculadora.py
@@ -0,0 +1,24 @@
+# Calculadora simples em python 
+
+A = input("digite um número:")
+B = input("digite um segundo número: ")
+
+##Operação de soma
+
+soma = int (A) + int(B)
+print("\n a soma é de A e B é igual a ", soma )
+
+#Operação de subtração
+
+subtração = int(A) - int(B)
+print("\n a subtração de A e B é igual a ", subtração )
+
+#Operação multiplicação
+
+multiplicação = int(A) * int(B)
+print("\n a smultiplicação de A e B é igual a ", multiplicação )
+
+#Operação Divisão
+
+divisão = int(A)/int(B)
+print("\n a divisão de A e B é igual a ", divisão)
